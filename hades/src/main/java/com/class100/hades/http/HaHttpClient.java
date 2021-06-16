00f74837f4f59728d211eebaf8cbce8189299180
Karic <karic@class100.com>
2021-06-16 09:51:34 +0800

    Disable validate https certificate

--- hades/src/main/java/com/class100/hades/http/HaHttpClient.java
+++ hades/src/main/java/com/class100/hades/http/HaHttpClient.java
@@ -6,7 +6,16 @@ import com.class100.atropos.generic.AtLog;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Headers;
 import okhttp3.Interceptor;
@@ -45,6 +54,8 @@ public final class HaHttpClient extends AtContextAbility {
         //    if (enableLog) {
         builder.addInterceptor(new LogInterceptor());
         //    }
+        builder.sslSocketFactory(SSLSocketClient.getSSLSocketFactory())
+                .hostnameVerifier(SSLSocketClient.getHostnameVerifier());
         dispatcher = new HaRequestDispatcher(builder.build());
     }
 
@@ -102,4 +113,47 @@ public final class HaHttpClient extends AtContextAbility {
             return response;
         }
     }
+
+    public static class SSLSocketClient {
+        public static SSLSocketFactory getSSLSocketFactory() {
+            try {
+                SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, getTrustManager(), new SecureRandom());
+                return sslContext.getSocketFactory();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static TrustManager[] getTrustManager() {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[]{};
+                        }
+                    }
+            };
+            return trustAllCerts;
+        }
+
+        public static HostnameVerifier getHostnameVerifier() {
+            HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            };
+            return hostnameVerifier;
+        }
+    }
+
 }
